2d223a5e99aa88483baf9fbe0ec2998074306b56
Ubuntu <pakshinweb@gmail.com>
2020-10-19 12:00:23 +0300

    add project

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
+##Deploy
 
+### Copy project
+```
+git clone https://github.com/pakshinweb/Comment.git
+```
+```
+cd Comment/
+```
+### Stop all
+```
+docker stop $(docker ps -q -a)
+```
+### Build
+```
+docker-compose build
+docker-compose up -d
+docker-compose exec php chown -R www-data:www-data ./
+docker-compose exec php mv .env.example .env
+docker-compose exec php php artisan key:generate
+docker-compose exec php php artisan migrate 
+```
---
+++ app/app/Http/Controllers/CommentController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\CommentStoreRequest;
+use App\Models\Category;
+use App\Models\Comment;
+use Illuminate\Auth\Events\Validated;
+
+class CommentController extends Controller
+{
+
+    public function index()
+    {
+        $comments = Comment::orderBy('id', 'DESC')->get();
+
+        return view('comment',['comments' => $comments]);
+    }
+
+    public function store(CommentStoreRequest $request)
+    {
+        $data = $request->validate([
+            'name' => 'required',
+            'comment' => 'required',
+        ]);
+
+        Comment::create($data);
+        return redirect()->route('home');
+    }
+
+}
